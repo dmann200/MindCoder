b0e7c9648d776a0e0df863e5f3eff6888bf1e35b
David Mann <david@ultramusician.com>
2019-10-07 18:38:52 -0700

    Initial Commit

---
+++ MindCoder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MindCoder
+//
+//  Created by Admin on 10/7/19.
+//  Copyright © 2019 UltraMusician. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MindCoder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MindCoder
+//
+//  Created by Admin on 10/7/19.
+//  Copyright © 2019 UltraMusician. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
